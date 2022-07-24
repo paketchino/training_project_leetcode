e50cfa953e0740b1de17d9547f9264bebee0e8c9
paketchino <chansgorn@gmail.com>
2022-07-24 23:57:42 +0300

    3.2. Откат изменений [#504830]

--- src/main/java/leetcode/PalindromeNumber.java
+++ src/main/java/leetcode/PalindromeNumber.java
@@ -3,12 +3,16 @@ package leetcode;
 public class PalindromeNumber {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(123123));
     }
 
     public static boolean isPalindrome(int x) {
         boolean rls = true;
         String number = Integer.toString(x);
+        if (number.length() <= 1) {
+            return true;
+        }
         int length = number.length();
         for (int i = 0; i < (length / 2); i++) {
             if (number.charAt(i) != number.charAt(length - i - 1)) {
